fb5a4274e424860680351c6b174be61f3885de08
Joseph Cameron <jf.cameron@gmail.com>
2019-09-28 21:31:10 -0400

    added more interfaces

---
+++ include/gdk/audio/listener.h
@@ -0,0 +1,12 @@
+// © 2019 Joseph Cameron - All Rights Reserved
+
+namespace gdk::audio
+{
+    /// \brief hears emitters in a 3d scene
+    class listener
+    {
+    public:
+        virtual ~listener() = default;
+    };
+}
+
---
+++ include/gdk/audio/scene.h
@@ -0,0 +1,16 @@
+// © 2019 Joseph Cameron - All Rights Reserved
+
+#ifndef GDK_AUDIO_OPENAL_STREAM_H
+#define GDK_AUDIO_OPENAL_STREAM_H
+
+namespace gdk::audio
+{
+    /// \brief 3d space filled with listeners and emitters etc.
+    class scene
+    {
+    public:
+        virtual ~scene() = default;
+    };
+}
+#endif
+
